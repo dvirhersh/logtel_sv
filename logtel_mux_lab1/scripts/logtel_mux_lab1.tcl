aac1044e1d9ecffc0ca8c7cfbd2c5aa51fccd66b
Dvir Hershkovits <dvirhersh@gmail.com>
2025-05-30 13:19:14 +0300

    rtl-files only in rtl dir
    Signed-off-by: Dvir Hershkovits <dvirhersh@gmail.com>

--- logtel_mux_lab1/scripts/logtel_mux_lab1.tcl
+++ logtel_mux_lab1/scripts/logtel_mux_lab1.tcl
@@ -128,8 +128,8 @@ if {[string equal [get_filesets -quiet sources_1] ""]} {
 set obj [get_filesets sources_1]
 # Import local files from the original project
 set files [list \
- [file normalize "${origin_dir}/../rtl/MUX.v"]\
- [file normalize "${origin_dir}/../rtl/MUX.sv"]\
+ [file normalize "${origin_dir}/rtl/MUX.v"]\
+ [file normalize "${origin_dir}/rtl/MUX.sv"]\
 ]
 set imported_files [import_files -fileset sources_1 $files]
 
